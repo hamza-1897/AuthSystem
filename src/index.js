2ba889d8b91fafff5e2ee14932364c78b1cc5ee0
Muhammad Hamza <hanjaa1897@gmail.com>
2026-03-15 15:33:41 +0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const morgan = require('morgan');
+const app = express();
+
+const port = 3000;
+
+app.use(express.json());
+app.use(morgan('dev'));
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
